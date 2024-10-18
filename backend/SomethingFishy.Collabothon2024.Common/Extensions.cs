c34fce2c81b8e7bd36c170168262933da9359d50
Mateusz Brawański <emzi0767@gmail.com>
2024-10-19 00:06:16 +0200

    Implement models and necessary conversions.

---
+++ backend/SomethingFishy.Collabothon2024.Common/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace SomethingFishy.Collabothon2024.Common;
+
+public static class Extensions
+{
+    public static JsonSerializerOptions WithCommerzConverters(this JsonSerializerOptions options)
+    {
+        options.Converters.Add(new CommerzPhoneTypeConverter());
+        options.Converters.Add(new CommerzAddressTypeConverter());
+        options.Converters.Add(new JsonStringEnumConverter());
+        return options;
+    }
+}
