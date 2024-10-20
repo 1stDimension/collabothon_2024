6013c7fe745849e7810857ea1e3a87f10d71f02b
Mateusz Brawański <emzi0767@gmail.com>
2024-10-20 10:04:57 +0200

    Fix AI search

--- AI_search_files_2.py
+++ AI_search_files_2.py
@@ -1,29 +1,29 @@
-from google.cloud import discoveryengine_v1
+from google.cloud import discoveryengine_v1beta as discoveryengine_v1
 import os
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "lodzkiterror-65599eb0142d.json"
 
 # Replace these placeholders with your actual values
 project_id = "lodzkiterror"
-location = "eu"
+location = "global"
 collection_id = "default_collection"
-engine_id = "genericagentingestion_1729302945333"
+engine_id = "genericagentingestion-glob_1729410060971"
 serving_config = "default_search"
 query = "how to make pizza"  # Replace with your actual search query
 
 # Authenticate using Google Cloud Application Default Credentials
-client = discoveryengine_v1.DiscoveryEngineClient()
+client = discoveryengine_v1.SearchServiceClient()
 
 # Construct the search request
 request = discoveryengine_v1.SearchRequest(
     query=query,
     page_size=10,
-    query_expansion_spec=discoveryengine_v1.QueryExpansionSpec(
-        condition=discoveryengine_v1.QueryExpansionSpec.Condition.AUTO
+    # query_expansion_spec=discoveryengine_v1.SearchRequest.QueryExpansionSpec(
+    #     condition=discoveryengine_v1.SearchRequest.QueryExpansionSpec.Condition.AUTO
+    # ),
+    spell_correction_spec=discoveryengine_v1.SearchRequest.SpellCorrectionSpec(
+        mode=discoveryengine_v1.SearchRequest.SpellCorrectionSpec.Mode.AUTO
     ),
-    spell_correction_spec=discoveryengine_v1.SpellCorrectionSpec(
-        mode=discoveryengine_v1.SpellCorrectionSpec.Mode.AUTO
-    ),
-    name=f"projects/{project_id}/locations/{location}/collections/{collection_id}/engines/{engine_id}/servingConfigs/{serving_config}:search"
+    serving_config=f"projects/{project_id}/locations/{location}/collections/{collection_id}/engines/{engine_id}/servingConfigs/{serving_config}"
 )
 
 # Send the request and get the response
@@ -31,6 +31,6 @@ response = client.search(request)
 
 # Process the response and extract the desired information
 for entity in response.results:
-    print(entity.name)
-    for property in entity.properties:
-        print(f"{property.key}: {property.value}")
+    print(entity.document.derived_struct_data["link"])
+    # for snippet in entity.document.derived_struct_data:
+    #     print(snippet)
